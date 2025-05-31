82d96bef79fa5e8964a853abc4de567daf8567fe
ainstarc <ainstarc@users.noreply.github.com>
2025-05-31 12:28:08 +0000

    deploy: 66481ef353ef37fec543177eb9a108fd19c4d2fb

---
+++ precache-manifest.dd125f534a7854b1b4b7d9e0ac56482d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08c04259fcd194180227c556452c1a8",
+    "url": "/git-init/index.html"
+  },
+  {
+    "revision": "f6cda04784a123fab6c9",
+    "url": "/git-init/static/css/main.8b11aea6.chunk.css"
+  },
+  {
+    "revision": "f2ba538b780f2220eda2",
+    "url": "/git-init/static/js/2.23fb4e5e.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/git-init/static/js/2.23fb4e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60fd695eaa8186314103",
+    "url": "/git-init/static/js/3.0adad4d9.chunk.js"
+  },
+  {
+    "revision": "f6cda04784a123fab6c9",
+    "url": "/git-init/static/js/main.9af86964.chunk.js"
+  },
+  {
+    "revision": "87469334a3a162ae4492",
+    "url": "/git-init/static/js/runtime-main.54594cc4.js"
+  }
+]);
