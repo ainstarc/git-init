c2684096e99ec2e13c68fbe7e6d793b2198922ea
ainstarc <ainstarc@users.noreply.github.com>
2025-06-07 03:30:12 +0000

    deploy: 901bdf56c80db6cd5af959f1327a39a573663114

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,94 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+/* eslint-disable no-restricted-globals */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+// This service worker can be customized!
+// See https://developers.google.com/web/tools/workbox/modules/workbox-precaching
+// for more information about precaching.
 
-importScripts(
-  "/git-init/precache-manifest.c5863112913afa8f06ed37160536b25f.js"
-);
+const CACHE_NAME = "git-init-cache-v1.2.0";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/static/js/main.bundle.js",
+  "/static/css/main.css",
+  "/manifest.json",
+  "/Git-Icon-1788C.png",
+];
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+// Install a service worker
+self.addEventListener("install", (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+  // Force the waiting service worker to become the active service worker
+  self.skipWaiting();
 });
 
-workbox.core.clientsClaim();
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+      return fetch(event.request)
+        .then((response) => {
+          // Check if we received a valid response
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== "basic"
+          ) {
+            return response;
+          }
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/git-init/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+          // Clone the response
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        })
+        .catch(() => {
+          // If the request is for a page, return the offline page
+          if (event.request.mode === "navigate") {
+            return caches.match("/offline.html");
+          }
+        });
+    })
+  );
+});
+
+// Update a service worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+          return null;
+        })
+      );
+    })
+  );
+  // Ensure the service worker takes control of all clients as soon as it's activated
+  self.clients.claim();
+});
+
+// Listen for messages from clients
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
